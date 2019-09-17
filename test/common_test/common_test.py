a97d4993819749c0251c62c0cc158b8ba70d8b0f
Zhe Yang <zhe.yang@cern.ch>
2019-09-17 17:09:42 +0200

    modify common_test.py to work in different device

--- test/common_test/common_test.py
+++ test/common_test/common_test.py
@@ -1,3 +1,4 @@
+import os
 import sys
 sys.path.append("..") # add self-defined module in the parent path
 sys.path.append("../..") # add self-defined module in the parent path
@@ -6,7 +7,7 @@ from lfv_pdnn_code_v1.common import common_utils
 
 print "*" * 80
 print "tests read_dict_from_txt"
-file_path = "/afs/cern.ch/work/y/yangz/public/lfv/lfv_pdnn_code_v1/test/common_test/dict_generate_test.txt"
+file_path = os.getcwd() + "/dict_generate_test.txt"
 dict_output = common_utils.read_dict_from_txt(file_path, key_type='float', value_type='float')
 print "output dictionary"
 print dict_output
