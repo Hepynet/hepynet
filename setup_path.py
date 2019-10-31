62c7ecf57b7e24236625b7e1b59a24189f096707
Zhe Yang <starprecursor@gmail.com>
2019-10-31 12:16:31 +0100

    Add path setup script.

--- setup_path.py
+++ setup_path.py
@@ -1,5 +1,14 @@
 #!/bin/env python
 
-import os, sys
+import os
+import site
+
+# add package path to python path
 package_path = os.getcwd()
-sys.path.insert(0, package_path)
+print(package_path)
+user_site_dir = site.getusersitepackages()
+if not os.path.exists(user_site_dir):
+  os.makedirs(user_site_dir)
+path_cfg_file = open(user_site_dir + "/pdnn.pth", 'w')
+path_cfg_file.write(package_path)
+path_cfg_file.close()
