7dbb846c59eda43e72df8b9a4f1b40905cc51c6f
Zhe Yang <starprecursor@gmail.com>
2019-11-05 16:51:34 +0100

    Add package installation script.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='lfv_pdnn',
+  version='0.0.1',
+  packages=find_packages(),
+  install_requires=[
+          'numpy>1.16',
+          'matplotlib>3.0',
+          'tensorflow>1.14',
+          'keras>2.3',
+          'scikit-learn>0.20',
+          'ConfigParser>4.0',
+          'reportlab>3.5'
+      ],
+  entry_points={
+      'console_scripts': [
+        'execute_pdnn_job=share.execute:main'
+      ],
+    },
+)
