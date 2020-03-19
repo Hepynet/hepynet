50f31600ff38712f5de591746982e04861c2faf6
Zhe Yang <zhe.yang@cern.ch>
2020-03-19 21:29:28 +0100

    Add config files

--- share/execute.py
+++ share/execute.py
@@ -4,18 +4,18 @@ import sys
 # Show tensorflow warnings and errors only
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 
-from lfv_pdnn.train.job_utils import *
+from lfv_pdnn.train import job_utils
 
-def main():
+def execute():
   argv_len = len(sys.argv)
   if argv_len <=1 :
     print('No ini file path specified!')
-    print('Usage: execute_pdnn_job path/to/ini-file')
+    print('Usage: execute_pdnn_jobs path/to/ini-file')
   elif argv_len >= 2:
     for i in range(1, argv_len):
       print('#'*80)
       print('Executing: ', sys.argv[i])
-      ex_test = job_executor(sys.argv[i])
+      ex_test = job_utils.job_executor(sys.argv[i])
       ex_test.get_config()
       ex_test.load_arrays()
       ex_test.execute_jobs()
