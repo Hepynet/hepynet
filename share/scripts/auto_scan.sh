d9f031edb85c39a46641ed074fd466e814f1ec66
Zhe Yang <starprecursor@gmail.com>
2020-05-04 14:08:32 +0200

    Updates config files

--- share/scripts/auto_scan.sh
+++
@@ -1,3 +0,0 @@
-execute_pdnn_jobs /work/share/train/emu_scan.ini
-execute_pdnn_jobs /work/share/train/etau_scan.ini
-execute_pdnn_jobs /work/share/train/mutau_scan.ini
--- share/scripts/auto_train.sh
+++
@@ -1,8 +0,0 @@
-execute_pdnn_jobs /work/share/train/emu_default.ini
-execute_pdnn_jobs /work/share/train/emu_zprime_default.ini
-
-execute_pdnn_jobs /work/share/train/etau_default.ini
-execute_pdnn_jobs /work/share/train/etau_zprime_default.ini
-
-execute_pdnn_jobs /work/share/train/mutau_default.ini
-execute_pdnn_jobs /work/share/train/mutau_zprime_default.ini
