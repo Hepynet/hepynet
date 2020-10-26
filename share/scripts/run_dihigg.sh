a016caa834a7a9446a4a8fe29133a692a4a0f92b
Zhe Yang <starprecursor@gmail.com>
2020-10-26 20:42:41 +0100

    Save auto-run scripts

--- share/scripts/run_dihigg.sh
+++ share/scripts/run_dihigg.sh
@@ -4,4 +4,4 @@ execute_pdnn_jobs /work/share/dihiggs/train/2tag_sr2.ini
 execute_pdnn_jobs /work/share/dihiggs/train/2tag_sr1_lmsf.ini
 execute_pdnn_jobs /work/share/dihiggs/train/2tag_sr1_lmdf.ini
 execute_pdnn_jobs /work/share/dihiggs/train/2tag_sr1_hmdf.ini
-execute_pdnn_jobs /work/share/dihiggs/train/2tag_sr1.ini
+#execute_pdnn_jobs /work/share/dihiggs/train/2tag_sr1.ini
