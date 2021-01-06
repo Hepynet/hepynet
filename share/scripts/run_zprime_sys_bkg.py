aa5fda2c13cbedab2266425a1514a1a7d3dbcb45
Zhe Yang <zhe.yang@cern.ch>
2021-01-06 16:41:46 +0100

    Update script

--- share/scripts/run_zprime_sys_bkg.py
+++ share/scripts/run_zprime_sys_bkg.py
@@ -1,4 +1,5 @@
 import subprocess
+import os
 
 bkg_ntuple_names = [
     "tree_NOMINAL",
@@ -201,5 +202,5 @@ bkg_ntuple_names = [
 command_template = "hepynet -d ./share/zprime/apply/high_mass_sys/high_mass_all-mass_apply_sys_bkg_{p_variation}.yaml"
 
 for variation in bkg_ntuple_names:
-    subprocess.call(command_template.format(p_variation=variation))
-
+    #subprocess.call()
+    os.system(command_template.format(p_variation=variation))
