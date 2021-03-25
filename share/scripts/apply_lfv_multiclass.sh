049658a1e821744583a2ceae06ce8cd2a8394ed6
Zhe Yang <starprecursor@gmail.com>
2021-03-25 02:02:09 +0100

    Remove share folder

--- share/scripts/apply_lfv_multiclass.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-execute_pdnn_jobs /work/share/lfv/apply/apply_emu_multiclass.ini
-execute_pdnn_jobs /work/share/lfv/apply/apply_etau_multiclass.ini
-execute_pdnn_jobs /work/share/lfv/apply/apply_mutau_multiclass.ini
